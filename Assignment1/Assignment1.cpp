faa759676b769f6ea2ba16c2c4a3475cc952199a
aGFja2Vy <42489638+aGFja2Vy@users.noreply.github.com>
2018-09-01 10:11:27 -0700

    Add files via upload

---
+++ Assignment1/Assignment1.cpp
@@ -0,0 +1,22 @@
+/*
+Name: Payton Erickson
+Student ID: 0545468
+Date: June 9, 2018
+Assignment #: 1
+Assignment Description: Basic outputs to the windows command line
+*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+// Output First and Last name with 2 line gap
+    cout<<"Payton"<<endl<<endl;
+    cout<<"Erickson"<<endl<<endl;
+//Output my email
+    cout<<"perickson468@student.msjc.edu"<<endl<<endl;
+//Output the due date
+    cout<<"June 10th 2018"<<endl<<endl;
+    return 0;
+}
---
+++ Assignment1/build/Debug/GNU-Linux/Assignment1.o.d
@@ -0,0 +1,1 @@
+build/Debug/GNU-Linux/Assignment1.o: Assignment1.cpp
